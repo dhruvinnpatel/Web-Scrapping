7101233c7a43d7bc8bb47056b8331761c87bfabe
dhruvin.patel <here.dhruvin@gmail.com>
2024-10-03 15:33:20 +0530

    code optimization

--- gem-Bid/pdf_data/scrap.py
+++ gem-Bid/pdf_data/scrap.py
@@ -21,7 +21,7 @@ def init_driver():
         "download.directory_upgrade": True,
         "safebrowsing.enabled": True
     }
-    # chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")
     chrome_options.add_experimental_option("prefs", prefs)
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
